e1b0a3751498a56a273562ed07f7c1c162ae1ca7
tanruixiang <tanruixiang0104@gmail.com>
2023-05-24 17:09:34 +0800

    hash benchmark data

--- common_types/src/hash.rs
+++ common_types/src/hash.rs
@@ -1,9 +1,12 @@
 // Copyright 2022 CeresDB Project Authors. Licensed under Apache-2.0.
 
 // custom hash mod
+use std::{collections, time::Instant};
+
+pub use ahash::AHasher;
 use byteorder::{ByteOrder, LittleEndian};
 use murmur3::murmur3_x64_128;
-
+use rand::Rng;
 pub fn hash64(mut bytes: &[u8]) -> u64 {
     let mut out = [0; 16];
     murmur3_x64_128(&mut bytes, 0, &mut out);
@@ -13,6 +16,10 @@ pub fn hash64(mut bytes: &[u8]) -> u64 {
 
 #[cfg(test)]
 mod test {
+    use std::hash::{Hash, Hasher};
+
+    use murmur3::murmur3_32;
+
     use super::*;
 
     #[test]
@@ -35,5 +42,44 @@ mod test {
             let res2 = hash64(&test_bytes_2);
             assert_ne!(res1, res2);
         }
+    }
+
+    #[test]
+    fn hash_benchmark() {
+        fn generate_random_string(length: usize) -> String {
+            let mut rng = rand::thread_rng();
+            let chars: Vec<char> = (0..length)
+                .map(|_| rng.gen_range(0..36))
+                .map(|n| if n < 26 { (n + 97) as u8 } else { (n - 26 + 48) as u8 } as char)
+                .collect();
+            chars.iter().collect()
+        }
+
+        fn test_hash_function<F>(hash_function: F, num_tests: usize, string_length: usize)
+        where
+            F: Fn(&str) -> u64,
+        {
+            let mut total_time = 0;
+            for _ in 0..num_tests {
+                let string_to_hash = generate_random_string(string_length);
+                let start_time = Instant::now();
+                hash_function(&string_to_hash);
+                let end_time = start_time.elapsed();
+                total_time += end_time.as_nanos();
+            }
+            let avg_time = total_time as f64 / num_tests as f64;
+            println!(
+                "Average time to hash a string of length {}: {} nanoseconds",
+                string_length, avg_time
+            );
+        }
+        let a_hash = |x: &str| -> u64 {
+            let mut hasher = AHasher::default();
+            x.hash(&mut hasher);
+            hasher.finish()
+        };
+        test_hash_function(a_hash, 10000, 10);
+        let murmur_hash = |x: &str| -> u64 { hash64(x.as_bytes()) };
+        test_hash_function(murmur_hash, 10000, 10)
     }
 }
